f74be66524c613ca66116526b62d6acab52f80ed
back-pack-man <b4ckpackman@gmail.com>
2020-06-16 14:49:55 +0900

    add index.vue

---
+++ pages/index.vue
@@ -0,0 +1,394 @@
+<template>
+  <html>
+      <head>
+         <meta charset="utf-8">
+         <title>FLOWER｜かわいいが届くお花便</title>
+         <meta name="description" content="あなた好みのお花がポストに届く定期便アプリ">
+         <meta  name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+      </head>
+
+      <body>
+         <section class="header">
+            <img src="~/assets/img/noimage.png" margin=0 auto="16px" width="131px" height="116px">
+            <div class="title">かわいいが届くお花便</div>
+            <p>* 北海道、沖縄、離島エリアは現在対象外です</p>
+         </section>
+
+         <section class="content1">
+         <h2>届くのは<br>あなた好みのお花だけ</h2>
+         <p>FLOWERのお花は季節やトレンドに合わせてプロがセレクトしたもの。雰囲気の異なる複数のお花から毎回好きなものが選べます。</p>
+         </section>
+
+         <section class="content2">
+         <h2>お花はポストに投函<br>忙しくても大丈夫</h2>
+         <p>お花は専用BOXに入れてポストに直接お届け。不在で受け取れなくても安心して使えます。</p>
+         </section>
+
+         <section class="content3">
+         <h2>FLOWERなら<br>いつもかわいく飾れる</h2>
+         <p>専用の花器を使うと、届いたお花をさっと生けるだけでかわいい空間のできあがり。お花のある暮らしがもっと楽しくなります。</p>
+         </section>
+
+         <section class="content4">
+            <h2>お部屋に飾りやすいサイズの<br>ブーケをワンコインで</h2>
+            <img src="~/assets/img/noimage2.png" margin=0 auto="16px" width="131px" height="116px">
+            <p>お花とグリーン5〜6本のブーケ</p>
+            <p class="a">500円／1回</p>
+            <p class="b">* 送料は360円（税別）です。<br>* 価格は税別です。</p>
+         </section>
+
+         <section class="content5">
+            <h2>月に2回の注文で、<br>お花がいつもある丁度いいペース</h2>
+            <img src="~/assets/img/noimage3.png" margin=0 auto="16px" width="131px" height="116px">
+         </section>
+
+         <section class="content6">
+            <h2>どんなお花が届くの？</h2>
+            <img src="~/assets/img/noimage11.png" margin=0 auto="16px" width="352px" height="257px">
+
+            <h3>かわいいお花だけ</h3>
+            <p>色やサイズなど、かわいくおしゃれに飾れるお花にこだわってセレクト。季節やトレンドに合わせてブーケを作っています。</p>
+
+            <h3>元気な状態で</h3>
+            <p>お花は注文を受けてから市場で直接仕入れているから新鮮。お花の元気を保つ専用BOXでお届けします。</p>
+
+            <h3>そのまま飾れる</h3>
+            <p>あらかじめ飾るのに最適な長さに茎をカットしてお届け。箱から出して、そのまま花器に入れるだけでOKです。</p>
+         </section>
+
+         <section class="content7">
+            <h2>かんたん、かわいい<br>FLOWERのための花の器</h2>
+            <p>初回限定25%OFF + 送料無料</p>
+            <img src="~/assets/img/noimage12.png" margin=0 auto="16px" width="400px" height="448px">
+
+            <div class="itembody">
+               <p class="a">お花を飾るのに欠かせないのが花器。でも、お花と花器の相性もさまざま。手持ちにぴったりのものがなくて、いまいちかわいく飾れない…なんてことも。そこで、誰でもかんたんに、かわいくお花を飾れる専用の花器をつくりました。お花のある暮らしが、もっと楽しみになりますように。</p>
+
+               <div class="itemimage">
+               <img src="~/assets/img/noimage4.png" margin=0 auto="16px" width="98px" height="116px">
+               <p>bell</p>
+               <img src="~/assets/img/noimage5.png" margin=0 auto="16px" width="98px" height="116px">
+               <p>moon</p>
+               <img src="~/assets/img/noimage6.png" margin=0 auto="16px" width="98px" height="116px">
+               <p>candle</p>
+               </div>
+
+               <div class="price">
+                  <p class="b">初回注文後<br>2週間以内</p>
+
+                  <div class="content">
+                     <p class="c">1,200円（送料無料）</p>
+                     <p class="d">通常価格 1,600円（送料別）</p>
+                  </div>
+               </div>
+
+               <p class="e">アプリ内でご購入いただけます。</p>
+            </div>
+
+         </section>
+
+         <section class="content8">
+            <h2>FLOWERだから続けられる、<br>お花のある暮らし</h2>
+
+            <img src="~/assets/img/noimage7.png" margin=0 auto="16px" width="131px" height="116px">
+            <h3>お世話がかんたん</h3>
+            <p>お花の栄養剤をセットでお届け。<br>こまめな水換えは不要です。</p>
+
+            <img src="~/assets/img/noimage8.png" margin=0 auto="16px" width="131px" height="116px">
+            <h3>アプリがお知らせ</h3>
+            <p>お花のお届けはアプリがお知らせ。<br>受け取り忘れの心配はありません。</p>
+
+            <img src="~/assets/img/noimage9.png" margin=0 auto="16px" width="131px" height="116px">
+            <h3>安心の保証つき</h3>
+            <p>万が一枯れたお花が届いても、<br>新しいものを無料で再送します。</p>
+         </section>
+
+         <section class="content9">
+            <img src="~/assets/img/noimage10.png" margin=0 auto="16px" width="131px" height="116px">
+            <h3 class="navy mt16">かわいいが届くお花便</h3>
+         </section>
+      
+         <section class="footer">
+             <img class="instagram" src="~/assets/img/noimage13.png" margin=0 auto="16px" width="64px" height="64px">
+             <div class="footcontent">
+                <p>お知らせnote</p>
+                <p>FAQ</p>
+                <p>プライバシーポリシー</p>
+                <p>利用規約</p>
+                <p>特定商取引法に関する表記</p>
+                <p>運営会社</p>
+               </div>
+
+             <p class="copyright">Copyright ROLLCAKE Inc. All Rights Reserved.</p>
+         </section>
+      </body>
+  </html>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+@media screen and (max-width: 480px){
+
+   body, h2, h3, h4, p {
+    font-family: "Noto Sans JP", sans-serif;
+    line-height: 1.6;
+    letter-spacing: 0.05rem;
+   }
+
+   body {
+    margin: 0;
+    padding: 0;
+    overflow-x: hidden;
+    min-width: 320px;
+    background: #fff;
+    color: #fff;
+   }
+
+  .header{
+    background-size: cover;
+    background-position: center;
+    height: 100vh;
+    min-height: 660px;
+    text-align: center;
+    position: relative;
+    padding-top: 48px;
+    background-color: pink;
+    color: #fff;
+   }
+
+   .header p{
+    font-size: 13px;
+    margin: 0 auto;
+    padding-top: 260px;
+    position: absolute;
+    bottom: 24px;
+    left: 0;
+    right: 0;
+   }
+
+  .content1, .content2, .content3{
+    background-size: cover;
+    background-position: center bottom;
+    min-height: 540px;
+    padding: 32px 24px;
+    color: #fff;
+   }
+
+  .content1 h2, .content2 h2, .content3 h2, .content6 h2, .content7 h2, .content8 h2{
+    font-size: 22px;
+    margin: 0 0 10;
+   }
+
+   .content1 p, .content2 p, .content3 p{
+    font-size: 15px;
+    margin: 0 0 10;
+   }
+
+
+  .content1{
+    background-color: rgb(221, 15, 187);
+   }
+
+   .content2{
+    background-color: rgb(92, 166, 235);
+   }
+
+
+   .content3{
+    background-color: rgb(228, 215, 44);
+   }
+   
+   .content4{
+    background-size: cover;
+    background-position: center bottom;
+    text-align: center;
+    padding: 64px 24px 80px;
+    min-height: 540px;
+    background-color: rgb(207, 206, 196);
+    color: #3f434a;
+   }
+
+   .content4 h2, .content5 h2{
+    font-size: 20px;
+    margin: 0 0 10;
+   }
+
+   .content4 p, .content4 p{
+    font-size: 15px;
+   }
+
+   .content4 p.a{
+    font-size: 20px;
+   }
+
+   .content4 p.b{
+    font-size: 13px;
+   }
+
+   .content5{
+    width: 400px;
+    height: 388px;
+    background-size: cover;
+    background-position: center bottom;
+    text-align: center;
+    padding: 64px 0 80px;
+    min-height: 540px;
+    color: #3f434a;
+   }
+
+   .content6{
+    width: 400px;
+    height: 874px;
+    background-size: cover;
+    background-position: center bottom;
+    text-align: center;
+    padding: 64px 24px 80px;
+    min-height: 540px;
+    color: #3f434a; 
+    background-color: rgb(207, 115, 235);
+   }
+
+   .content6 h3{
+    font-size: 20px;
+    margin: 24px 0 10px;
+   }
+
+   .content6 p{
+    font-size: 15px;
+    margin: 0 0 15;
+    text-align: justify;
+   }
+
+   .content7{
+    width: 400px;
+    height: 1108px;
+    background-size: cover;
+    background-position: center bottom;
+    text-align: center;
+    padding: 64px 0 80px;
+    min-height: 540px; 
+    background-color: rgb(243, 233, 140);
+    color: #3f434a;
+   }
+
+   .content7 p{
+    font-size: 14px;
+    padding: 4 8;
+    margin: 0 73;
+   }
+
+   .content7 .itembody{
+      padding: 8px 24px;
+   }
+
+   .content7 p.a{
+    font-size: 15px;
+    margin: 15px 24px;
+    text-align: justify;
+   }
+
+   .itemimage{
+    display: flex;
+    margin: 24px 0;
+   }
+
+   .itemimage p{
+    font-size: 13px;
+    text-align: center;
+   }
+
+   .price{
+    display: flex;
+    justify-content: space-between;
+    margin: 0 0 16px;
+   }
+
+   .price .content{
+     order: 2;
+     display: block;
+   }
+
+   .price .content p.c{
+     font-size: 18px;
+   }
+
+   .price .content p.d{
+     font-size: 13px;
+   }
+
+   .price p.b{
+    width: 84px;
+    height: 56px;
+    font: 14px;
+    margin: 0 16px 0 0;
+    border: 2px solid;
+   }
+
+   .content8{
+    width: 400px;
+    height: 805px;
+    background-size: cover;
+    background-position: center bottom;
+    text-align: center;
+    padding: 64px 16px 80px;
+    min-height: 540px; 
+    color: #3f434a;
+   }
+
+   .content8 h3{
+    font-size: 20px;
+    margin: 0 0 10px;
+   }
+
+   .content9{
+    width: 400px;
+    height: 447px;
+    background-size: cover;
+    background-position: center bottom;
+    text-align: center;
+    padding: 64px 16px 0px;
+    min-height: 540px; 
+    background-color: rgb(243, 233, 140);
+    color: #3f434a;
+   }
+
+   .content9 h3{
+    font-size: 16px;
+    margin: 16px 0 24px;
+   }
+
+   .footer {
+    width: 400px;
+    height: 437px;
+    padding: 32px 32px 128px;
+    background: #3f434a;
+    position: relative;
+    min-height: 164px;
+   } 
+
+   .footer .footcontent{
+      font-size: 14px;
+      margin: 0 0 48px;
+   }
+
+  .footer p.copyright{
+      font-size: 13px;
+      color: #FFFFFF;
+      opacity: 0.4;
+   }
+
+   .footer .instagram{
+      position: absolute;
+      top: 24px;
+      bottom: 348px;
+      right: 24px;
+      left: 312px;
+   }
+
+}
+ 
+</style>
